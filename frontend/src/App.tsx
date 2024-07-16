4fdaba4e1a14146fdb5cf55131e7be314e8c9b59
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-15 23:14:31 -0700

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import boyFight from "./assets/boy-fight.jpg";
+import logo from "./assets/logo-red.png";
 
 function App() {
   return (
@@ -12,6 +13,29 @@ function App() {
           alt="Fighter boy"
         />
         <div className="absolute top-0 left-0 w-full h-full gradient-hero z-20"></div>
+        <div
+          className={
+            "absolute w-full flex flex-row top-2 text-lg z-40 justify-between p-4"
+          }
+        >
+          <div>
+            <img src={logo} alt="Logo" className={"w-12"} />
+          </div>
+          <div className={"flex flex-row items-center "}>
+            <ul className={"flex flex-row gap-4 m-0 p-0 list-none"}>
+              <li>Home</li>
+              <li>About</li>
+              <li>Locations</li>
+              <li>Contact</li>
+            </ul>
+            <button
+              className={"ml-4 bg-red-700 p-1.5"}
+              style={{ borderRadius: "2px" }}
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
