fcffe1b22cd931a99433cc6e894da93b933737c7
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-15 22:50:37 -0700

    added fighter box with gradient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import boyFight from "./assets/boy-fight.jpg";
 
 function App() {
-
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <div className={"w-lvw h-lvh"}>
+      <div
+        className={`relative flex items-end justify-end text-white bg-black text-2xl w-lvw h-3/5`}
+      >
+        <img
+          src={boyFight}
+          className={"object-fill w-auto h-full z-10"}
+          alt="Fighter boy"
+        />
+        <div className="absolute top-0 left-0 w-full h-full gradient-hero z-20"></div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
